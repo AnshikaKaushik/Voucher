04522a7a7f32a93012b02ee85759a0b6fec39863
AnshikaKaushik <67686107+AnshikaKaushik@users.noreply.github.com>
2020-07-01 15:31:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,18 +12,6 @@ app.use(bodyParser.json());
 const PORT = 5050;
 const MAX_TX = 5;
 
-    // const obj = {
-    //     voucher_amount : voucher_amount,
-    //     current_amount: voucher_amount,
-    //     email: email,
-    //     code: codeGenerator(),
-    //     voucher_pin: generatePin(),
-    //     voucher_createdAt: new Date(),
-    //     status: 'active',
-    //     transaction_count: 0,
-    //     last_transaction: null
-    // };
-
 app.post('/generate-voucher', (req, res) => {
     const email = req.body.email;
     const voucher_amount = req.body.voucher_amount;
@@ -46,7 +34,6 @@ app.post('/generate-voucher', (req, res) => {
     };
 
     vouchers.insertOne(obj);
-    // call function to send email.
 
     const message = "Voucher generated, code = "+obj.code+ " pin = "+obj.voucher_pin+ " amount = "+obj.voucher_amount ;
     sendMessage(message);
@@ -68,9 +55,9 @@ app.post('/redeem-voucher', (req, res) => {
     if(verifyEmail(email)) {
         const record = vouchers.findOne({code: voucher_code});
         record.transaction_count++;
-
+        
         /**
-         * Check maximum transaction
+         * Checking maximum transaction
          */
 
         if(record.transaction_count > MAX_TX) {
@@ -82,24 +69,19 @@ app.post('/redeem-voucher', (req, res) => {
         }
 
         /**
-         * Check last transaction time
+         * Checking last transaction time
          */
         
-
         const current_time = new Date();
         if(record.last_transaction > 0 && (record.last_transaction - current_time <=10)) {
             res.send({
-                message: "Please try after 10 minutes "
+                message: "Please try again after 10 minutes "
             });
-
             exit();
-
         } 
 
         if(record.current_amount > 0){
-
-         record.current_amount = record.current_amount - redeem_amount;
-
+            record.current_amount = record.current_amount - redeem_amount;
             record.status = 'partially active';
             record.last_transaction = new Date();
 
@@ -108,17 +90,13 @@ app.post('/redeem-voucher', (req, res) => {
 
         } else {
             record.status = 'redeemed';
-
             const message = "Voucher redeemed fully, code = "+voucher_code+ " Redeem Amount = "+redeem_amount;
             sendMessage(message);
 
         }
 
         vouchers.update(record);
-
-        /**
-         * create a string(message), function(message);
-         */
+        
         res.send({
             message: 'Redemption successfull.'
         })
@@ -129,20 +107,11 @@ app.post('/redeem-voucher', (req, res) => {
     }
 });
 
-/**
- * URL format - http://localhost:5050/get-voucher?email=abc@xyz.com&status=active
- */
 app.get('/get-voucher', async (req, res) => {
     const from = req.query.from;
     const to = req.query.to;
-
-    // if to is not present in the query , then assume 'to' value to current time,
-    // if from is not available and to is  available then ignore time filter.
-
-
     const email = req.query.email;
     const status = req.query.status;
-    // const time = vouchers.findOne({code: voucher_createdAt});
     let query = {};
         if(from) {
             if(!to) {
@@ -167,8 +136,6 @@ app.get('/get-voucher', async (req, res) => {
         if(status) {
             query.status = status;
         }
-
-        console.log(query);
 
         const response = await vouchers.find(query);
         res.send(response);
